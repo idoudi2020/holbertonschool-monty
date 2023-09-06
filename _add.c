c54ebb19cc4e4c3408255035ac6e68fd7010d7a4
saima-riaz <saimariaz1422@gmail.com>
2023-09-06 07:38:11 -0700

    fixed betty

--- _add.c
+++ _add.c
@@ -9,11 +9,11 @@
  */
 void _add(stack_t **stack, unsigned int line_number)
 {
-  if (!stack || !*stack || !(*stack)->next)
-    {
-      fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
-      exit(EXIT_FAILURE);
-    }
-  (*stack)->next->n += (*stack)->n;
-  _pop(stack, line_number);
+	if (!stack || !*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n += (*stack)->n;
+	_pop(stack, line_number);
 }
