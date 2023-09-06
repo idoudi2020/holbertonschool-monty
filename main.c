7a27bc648f2a1f047c0c2b137b6028e1320004cf
idoudi2020 <idoudimokhtar@gmail.com>
2023-09-06 09:36:02 -0700

    file

--- main.c
+++ main.c
@@ -1,45 +1,29 @@
 #include "monty.h"
 
 /**
- * main - entry point
- * @argc: The number of arguments passed to the program
- * @argv: An array of strings where each string is\
- *a command line argument
+ * main - Main entry point for the Monty code interpreter.
  *
- * Return: EXIT_SUCCESS on success or EXIT_FAILURE on failure
+ * @argc: The number of command line arguments.
+ * @argv: The command line arguments.
+ *
+ * Return: The exit status of the program.
  */
-int main(int argc, char *argv[])
+
+int main(int argc, char **argv)
 {
-	FILE *file;
-	char *line = NULL;
-	char *opcode = NULL;
-	size_t len = 0;
-	ssize_t read;
-	unsigned int line_number = 0;
-	stack_t *stack = NULL;
+  if (argc != 2)
+    {
+      fprintf(stderr, "USAGE: monty file\n");
+      exit(EXIT_FAILURE);
+    }
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	file = fopen(argv[1], "r");
+  stack_t *stack = NULL;
+  FILE *file = open_file(argv[1]);
 
-	if (file == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+  execute_instructions(file, &stack);
 
+  fclose(file);
+  free_stack(&stack);
 
-	for (; (read = getline(&line, &len, file)) != -1; line_number++)
-
-	{
-		opcode = strtok(line, " \n\t");
-		execute(opcode, line_number, &stack);
-	}
-	fclose(file);
-	free(line);
-	free_stack(&stack);
-	return (EXIT_SUCCESS);
+  return (EXIT_SUCCESS);
 }
--- monty.h
+++ monty.h
@@ -1,51 +1,36 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-/**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
- */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+  int n;
+  struct stack_s *prev;
+  struct stack_s *next;
 } stack_t;
 
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+  char *opcode;
+  void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void execute(char *opcode, unsigned int line_number, stack_t **stack);
-stack_t *new_node(int n);
+
+int _isdigit(char *str);
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
-int is_all_digits(char *arg);
-void free_stack(stack_t **stack);
 void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void _swap(stack_t **stack, unsigned int line_number);
 void _add(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);
-
-#endif
+void add_node(stack_t **stack, int n);
+void free_stack(stack_t **stack);
+void execute_instructions(FILE *file, stack_t **stack);
+FILE* open_file(char *filename);
+int parse_arg(char *arg, unsigned int line_number);
+#endif /* MONTY_H */
