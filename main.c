29a38a5531e26132bb051e543180c6834da7db5f
idoudi2020 <idoudimokhtar@gmail.com>
2023-09-06 09:45:48 -0700

    add file

--- main.c
+++ main.c
@@ -11,17 +11,15 @@
 
 int main(int argc, char **argv)
 {
+
+stack_t *stack = NULL;
   if (argc != 2)
     {
       fprintf(stderr, "USAGE: monty file\n");
       exit(EXIT_FAILURE);
     }
-  if (file == NULL)
-    {
-      fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-      exit(EXIT_FAILURE);
-    }
-  stack_t *stack = NULL;
+
+
   FILE *file = open_file(argv[1]);
 
   execute_instructions(file, &stack);
