fa1b37a17f5d5fa5db0c2366bf752a30d473e50b
saima-riaz <saimariaz1422@gmail.com>
2023-09-06 07:47:28 -0700

    betty fixed

--- execute.c
+++ execute.c
@@ -8,32 +8,32 @@
  */
 void execute(char *opcode, unsigned int line_number, stack_t **stack)
 {
-  instruction_t op_func[] = {
-			     {"push", _push},
-			     {"pall", _pall},
-			     {"pint", _pint},
-			     {"pop", _pop},
-			     {"swap", _swap},
-			     {"add", _add},
-			     {"nop", _nop},
-			     {NULL, NULL}
-  };
-  int i = 0;
+	instruction_t op_func[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"add", _add},
+		{"nop", _nop},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-  if (!opcode)
-    {
-      return;
-    }
-  while (op_func[i].opcode)
-    {
-      if (strcmp(opcode, op_func[i].opcode) == 0)
+	if (!opcode)
 	{
-	  op_func[i].f(stack, line_number);
-	  return;
+		return;
 	}
-      i++;
-    }
+	while (op_func[i].opcode)
+	{
+		if (strcmp(opcode, op_func[i].opcode) == 0)
+		{
+			op_func[i].f(stack, line_number);
+			return;
+		}
+		i++;
+	}
 
-  fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-  exit(EXIT_FAILURE);
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+	exit(EXIT_FAILURE);
 }
