a5d2343ae4fd9c13daaf98f879db5661f2f27178
saima-riaz <saimariaz1422@gmail.com>
2023-09-06 07:48:53 -0700

    fixed betty problem

--- free_stack.c
+++ free_stack.c
@@ -9,12 +9,12 @@
  */
 void free_stack(stack_t **stack)
 {
-  stack_t *tmp;
+	stack_t *tmp;
 
-  while (*stack != NULL)
-    {
-      tmp = (*stack)->next;
-      free(*stack);
-      *stack = tmp;
-    }
+	while (*stack != NULL)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+	}
 }
