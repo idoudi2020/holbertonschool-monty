3757885e5feae0f114841c1a716bd2e97a22fc4f
saima-riaz <saimariaz1422@gmail.com>
2023-09-06 07:55:36 -0700

    fixed the main function error

--- main.c
+++ main.c
@@ -10,36 +10,36 @@
  */
 int main(int argc, char *argv[])
 {
-  FILE *file;
-  char *line = NULL;
-  char *opcode = NULL;
-  size_t len = 0;
-  ssize_t read;
-  unsigned int line_number = 0;
-  stack_t *stack = NULL;
+	FILE *file;
+	char *line = NULL;
+	char *opcode = NULL;
+	size_t len = 0;
+	ssize_t read;
+	unsigned int line_number = 0;
+	stack_t *stack = NULL;
 
-  if (argc != 2)
-    {
-      fprintf(stderr, "USAGE: monty file\n");
-      exit(EXIT_FAILURE);
-    }
-  file = fopen(argv[1], "r");
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
 
-  if (file == NULL)
-    {
-      fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-      exit(EXIT_FAILURE);
-    }
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
 
-  for (; (read = getline(&line, &len, file)) != -1; line_number++)
+	for (; (read = getline(&line, &len, file)) != -1; line_number++)
 
-    {
-      opcode = strtok(line, " \r\n\t");
-      execute(opcode, line_number, &stack);
-    }
-  fclose(file);
-  free(line);
-  free_stack(&stack);
-  return (EXIT_SUCCESS);
+	{
+		opcode = strtok(line, " \r\n\t");
+		execute(opcode, line_number, &stack);
+	}
+	fclose(file);
+	free(line);
+	free_stack(&stack);
+	return (EXIT_SUCCESS);
 }
