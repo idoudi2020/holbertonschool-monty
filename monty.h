a8d55a73fe79a2f3eddc6b5ba4efd02ed25be946
saima-riaz <saimariaz1422@gmail.com>
2023-09-06 14:03:28 -0700

    header file fixed

--- monty.h
+++ monty.h
@@ -8,15 +8,15 @@
 
 typedef struct stack_s
 {
-  int n;
-  struct stack_s *prev;
-  struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 typedef struct instruction_s
 {
-  char *opcode;
-  void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 
