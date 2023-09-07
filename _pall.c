17292b191e679c23c0cb0709a08b7de0e5e2f64e
saima-riaz <saimariaz1422@gmail.com>
2023-09-07 04:37:38 -0700

    fixed block comment

--- _pall.c
+++ _pall.c
@@ -1,9 +1,10 @@
 #include "monty.h"
 /**
- *  * _pall - Prints all values in the stack from the top.
- *   * @stack: Pointer to a pointer to the stack.
- *    * @line_number: The line number (unused in this function).
- *     */
+ *_pall - Prints all values in the stack from the top.
+ * @stack: Pointer to a pointer to the stack.
+ * @line_number: The line number (unused in this function).
+ */
+
 void _pall(stack_t **stack, unsigned int line_number)
 {
 	(void)line_number;
