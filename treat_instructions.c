ce663d9a81710f70bf214b4632b5d6b1080d6fa9
saima-riaz <saimariaz1422@gmail.com>
2023-09-07 04:17:31 -0700

    treat errors

--- treat_instructions.c
+++ treat_instructions.c
@@ -1,7 +1,7 @@
-#include "monty.h"
 
 /**
- * treat_instructions - Reads file line by line executes Monty instructions
+ * execute_instructions - Reads a file line by line and executes Monty instructions.
+ *
  * @file: Pointer to the file to be read.
  * @stack: Double pointer to the stack.
  */
@@ -9,17 +9,22 @@ void treat_instructions(FILE *file, stack_t **stack)
 {
 	char *line = NULL;
 	size_t len = 0;
-	size_t read;
+	ssize_t read;
 	char *opcode;
-	unsigned int line_num = 0;
+	unsigned int line_number = 0;
+
 	static const instruction_t instructions[] = {
-		{"push", _push}, {"pall", _pall},
-		{"pint", _pint}, {"pop", _pop},
-		{"swap", _swap}, {"add", _add},
-		{"nop", _nop}, {NULL, NULL}
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"add", _add},
+		{"nop", _nop},
+		{NULL, NULL}
 	};
 
-	for (line_num = 1; (read = getline(&line, &len, file)) != -1; line_num++)
+	for (line_number = 1; (read = getline(&line, &len, file)) != -1; line_number++)
 	{
 
 		line[strcspn(line, "\n")] = '\0';
@@ -29,22 +34,22 @@ void treat_instructions(FILE *file, stack_t **stack)
 			continue;
 
 		int i;
-
 		for (i = 0; instructions[i].opcode; i++)
 		{
 			if (strcmp(opcode, instructions[i].opcode) == 0)
 			{
-				instructions[i].f(stack, line_num);
+				instructions[i].f(stack, line_number);
 				break;
 			}
 		}
 
 		if (instructions[i].opcode == NULL)
 		{
-			fprintf(stderr, "L%d: unknown instruction %s\n", line_num, opcode);
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
 			exit(EXIT_FAILURE);
 		}
 	}
 
 	free(line);
 }
+
